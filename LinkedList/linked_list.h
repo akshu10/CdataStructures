d2920c5160130ea80f75daaf2f99cc1d0c662cb4
Parth Patel <58887946+akshu10@users.noreply.github.com>
2020-11-09 22:33:53 -0400

    Add files via upload

---
+++ LinkedList/linked_list.h
@@ -0,0 +1,51 @@
+// This is the header file for the linkedlist contract.
+
+#ifndef LINKEDLIST_LINKED_LIST_H
+#define LINKEDLIST_LINKED_LIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+
+
+typedef struct _Node {
+
+    void *data;
+    struct _Node *next;
+    struct _Node *prev;
+} Node;
+
+typedef struct _LList {
+
+    Node *first;
+    Node *last;
+    int size;
+    int itemSize;
+    char *type;
+
+} LinkedList;
+
+
+LinkedList *llist_initialize(int, char *);
+
+bool llist_add_at(LinkedList *, int, void *);
+
+bool llist_add_first(LinkedList *, void *);
+
+bool llist_add_last(LinkedList *, void *);
+
+void *llist_get(LinkedList *, int);
+
+int llist_index_of(LinkedList *, void *);
+
+void *llist_remove(LinkedList *, int);
+
+void *llist_remove_first(LinkedList *);
+
+void *llist_remove_last(LinkedList *);
+
+bool llist_destroy(LinkedList *);
+
+#endif //LINKED_LIST_H
